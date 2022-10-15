eecaa16545f84814d28132a9dd0519b35e23d3ac
bakht1n@yandex.ru <bakht1n@yandex.ru>
2022-10-15 18:54:16 +0300

    adead about task1

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] ar1 = new string[5] {"end", "del", "hello", "world", "res"};
+string[] ar2 = new string[ar1.Length];
+void SecondArray(string[] ar1, string[] ar2)
+{
+    int count = 0;
+    for (int i = 0; i < ar1.Length; i++)
+    {
+    if(ar1[i].Length <= 3)
+        {
+        ar2[count] = ar1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(ar1, ar2);
+PrintArray(ar2);
